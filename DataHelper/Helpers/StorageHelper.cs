f529ac3bcf5da6ff828a02b89b477d2e599eca50
yvanwang <yffswyf@live.com>
2015-09-09 00:48:41 +0800

    Something Wrong about Task/IEumerable in GetAllFiles

--- DataHelper/Helpers/StorageHelper.cs
+++ DataHelper/Helpers/StorageHelper.cs
@@ -7,14 +7,61 @@ using Windows.Storage;
 
 namespace DataHelper.Helpers
 {
-    public class StorageHelper
+    public sealed class StorageHelper
     {
+        //and we can user Roaming;
+        
+        //Local Operate to Folder or File      
         static StorageFolder LocalStorageFolder = ApplicationData.Current.LocalFolder;
-        static ApplicationDataContainer LocalStorageSettiings = ApplicationData.Current.LocalSettings;
 
-        public static void SetValue()
+        //Local Setting for Save or Get Setting Value
+        static ApplicationDataContainer LocalStorageSettiings = ApplicationData.Current.LocalSettings;
+        
+        //Set Settings with key/value to Settings
+        public static void SetValueWithKey(string key, object value)
         {
-           
+            if (LocalStorageSettiings.Values.ContainsKey(key))
+            {
+                LocalStorageSettiings.Values[key] = value;
+            }
+            else
+            {
+                LocalStorageSettiings.Values.Add(key, value);
+            }
         }
+
+        //Get Value from settings
+        public static object GetValueWithKey(string key)
+        {
+            if (LocalStorageSettiings.Values.ContainsKey(key))
+            {
+                var value = LocalStorageSettiings.Values[key];
+                if (value != null)
+                    return value;
+                else
+                    return null;
+            }
+            else
+                return null;
+        }
+
+        //public static async IEnumerable<StorageFile> GetAllFiles()
+        //{ 
+        //    var folders = await LocalStorageFolder.GetFoldersAsync();
+        //    if (folders != null)
+        //    {
+        //        foreach (var folder in folders)
+        //        {
+        //            var files = await folder.GetFilesAsync();
+        //            if (files != null)
+        //            {
+        //                foreach (var file in files)
+        //                {
+        //                    yield return file;
+        //                }
+        //            }
+        //        }
+        //    }
+        //}
     }
 }
