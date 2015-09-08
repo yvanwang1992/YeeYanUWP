21818c790d1ac385524c8f81fb5681f0521bd191
YvanWang <yffswyf@live.com>
2015-09-08 19:01:05 +0800

    add StorageHelper Folder

---
+++ DataHelper/Helpers/StorageHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+
+namespace DataHelper.Helpers
+{
+    public class StorageHelper
+    {
+        static StorageFolder LocalStorageFolder = ApplicationData.Current.LocalFolder;
+        static ApplicationDataContainer LocalStorageSettiings = ApplicationData.Current.LocalSettings;
+
+        public static void SetValue()
+        {
+           
+        }
+    }
+}
